f7b7562ff0ad89000b0e5c9b51d5d5e710e78b7d
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-05-15 08:43:46 +0200

    Update main.go

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
+func Hello(v string) (string, error) {
+	if v == "hello" {
+		return "", errors.New("empty name")
+	}
+
+	return v, nil
+}
+
 func main() {
-	fmt.Println("Running cli")
+	fmt.Println(Hello("hello"))
 }
